9fa6de758583cc002af39a506c2edf53a9e7a503
fvergarh <fvergarh@SAN-H9PC162.usersad.everis.int>
2017-04-14 19:27:43 -0300

    agregando conexion a base de dadtos y prueba de split de paquete recibido

--- src/udpServer/UDPServer.java
+++ src/udpServer/UDPServer.java
@@ -5,10 +5,56 @@
  */
 package udpServer;
 
-/**
- *
- * @author fvergarh
- */
-public class UDPServer {
-    
+import java.io.*;
+import java.net.*;
+import java.sql.*;
+
+class UDPServer {
+
+    private static Connection cnx = null;
+
+    public static Connection obtener() throws SQLException, ClassNotFoundException {
+        if (cnx == null) {
+            try {
+                Class.forName("com.mysql.jdbc.Driver");
+                cnx = DriverManager.getConnection("jdbc:mysql://localhost/java_mysql", "root", "");
+            } catch (SQLException ex) {
+                throw new SQLException(ex);
+            } catch (ClassNotFoundException ex) {
+                throw new ClassCastException(ex.getMessage());
+            }
+        }
+        return cnx;
+    }
+
+    public static void cerrar() throws SQLException {
+        if (cnx != null) {
+            cnx.close();
+        }
+    }
+
+    public static void main(String args[]) throws Exception {
+        DatagramSocket serverSocket = new DatagramSocket(9876);
+        byte[] receiveData = new byte[1024];
+        byte[] sendData = new byte[1024];
+        while (true) {
+            DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+            serverSocket.receive(receivePacket);
+            String sentence = new String(receivePacket.getData());
+            String[] fragmento = sentence.split(",");
+
+            for (int i = 0; i < fragmento.length; i++) {
+                System.out.println(i + ": " + fragmento[i]);
+            }
+
+            System.out.println("RECEIVED: " + sentence);
+            InetAddress IPAddress = receivePacket.getAddress();
+            int port = receivePacket.getPort();
+            String capitalizedSentence = sentence.toUpperCase();
+            sendData = capitalizedSentence.getBytes();
+            DatagramPacket sendPacket
+                    = new DatagramPacket(sendData, sendData.length, IPAddress, port);
+            serverSocket.send(sendPacket);
+        }
+    }
 }
